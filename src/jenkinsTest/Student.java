f174fa83367185c59fbe4cd16a2442c59244dc30
Jake Duggan <c00221072@itcarlow.ie>
2019-01-04 15:17:05 +0000

    added absent grade

--- src/jenkinsTest/Student.java
+++ src/jenkinsTest/Student.java
@@ -4,12 +4,13 @@ package jenkinsTest;
  * Created by meudecc on 15/02/2017.
  */
 public class Student {
-	//test commit
+
     public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
-        if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance == 0) return AttendanceGrade.ABSENT;
+        if (attendance < 30 && attendance >= 1) return AttendanceGrade.VERY_POOR;
         if (attendance < 50) return AttendanceGrade.POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
         if (attendance <= 90) return AttendanceGrade.GOOD;
